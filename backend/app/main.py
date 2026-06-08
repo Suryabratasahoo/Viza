e827ad167c69777e39ce9278315d9989c5c0faf8
Surya <suryabratasahoo882@gmail.com>
2026-06-08 18:02:18 +0530

    Update CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.upload import router as upload_router
-from app.routes.datasets import router as dataset_router
+from app.routes.datasets import router as datasets_router
 from app.routes.query import router as query_router
 from app.routes.context import router as context_router
 from app.routes.ask import router as ask_router
 from app.routes.auth_router import router as auth_router
 from app.routes.dataset_router import router as dataset_router
 from app.routes.chat_session_router import router as chat_session_router
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "https://viza-nine.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -22,12 +23,12 @@ app.add_middleware(
 )
 
 app.include_router(upload_router)
+app.include_router(datasets_router)
 app.include_router(dataset_router)
 app.include_router(query_router)
 app.include_router(context_router)
 app.include_router(ask_router)
 app.include_router(auth_router)
-app.include_router(dataset_router)
 app.include_router(chat_session_router)
 
 @app.get("/")
